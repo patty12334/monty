51299a9abcbf9efae1057b4ed37bb8d2f9697e9b
St-Pardon <okc4pardon@gmail.com>
2022-07-13 14:46:10 +0000

    push and pall

---
+++ main.c
@@ -0,0 +1,78 @@
+#include "monty.h"
+
+static stack_t *Values;
+static char **Lines;
+static int Count;
+static int Curr_Line;
+static char Data_Format;
+
+/**
+ * main - Entry_ Monty ByteCode interpreter
+ * @argc: lenght of arguments
+ * @argv: array of arguments
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		Data_Format = DF_LIFO;
+		Lines = read_file(argv[1], &Count);
+		while (Curr_Line < Count)
+		{
+			_execute(Lines[Curr_Line], Curr_Line + 1, &Values);
+			Curr_Line++;
+		}
+	}
+	else
+	{
+		write(STDERR_FILENO, "USAGE: monty file\n", 18);
+		exit(EXIT_FAILURE);
+	}
+	clean_mem();
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * clean_up_program - Frees dynamically allocated memory blocks
+ */
+
+void clean_mem(void)
+{
+	stack_t *node = Values, *tmp = NULL;
+	int i;
+
+	if (Lines != NULL)
+	{
+		for (i = 0; i < Count; i++)
+		{
+			if (Lines[i] != NULL)
+				free(Lines[i]);
+		}
+		if (Lines != NULL)
+			free(Lines);
+	}
+	if (node != NULL)
+	{
+		while ((node != NULL) && (node->prev != NULL))
+			node = node->prev;
+		while (node != NULL)
+		{
+			tmp = node->next;
+			free(node);
+			node = tmp;
+		}
+	}
+}
+
+/**
+ * exit_program - exits the program
+ * @status: The exit code
+ */
+
+void exit_(int status)
+{
+	clean_mem();
+	exit(status);
+}
